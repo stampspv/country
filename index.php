6e3d36159d10a2727f2a402fc1b7f8b77b8ab7da
stampspv <suphavich.c@gmail.com>
2018-02-06 15:35:12 +0700

    auto complete
    auto complete

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
-<!-- Latest compiled and minified CSS & JS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <script src="//code.jquery.com/jquery.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+<script src="jquery.easy-autocomplete.min.js"></script>
+<link rel="stylesheet" href="easy-autocomplete.min.css">
 <style type="text/css">
+body{
+	font-family: 'Raleway', sans-serif;
+}
 .page{
 	background-color: #705d5d;
-	color:white;
 	margin-top: 20px;
 	padding: 20px;
+	border-bottom:10px solid #a88b8c;
 }
 .submtbtn{
 	background-color: #ffdc19;
 	color:black;
-	font-size: 1.5em;
+	font-size: 1.2em;
 	padding: 5px 30px;
 	transition: 0.5s;
 }
@@ -24,34 +29,41 @@
 .coloroftext{
 	color:#FFC600;
 }
+.page h1,label,.colorinfo{
+	color:white;
+}
 </style>
+<body>
 <div class="row">
 	<div class="col-lg-2"></div>
 	<div class="col-lg-8 page">
 		<h1>Takes a country name to see a capital of the country.</h1>
-		<form action="index.php" method="GET" role="form">
+		<form action="index.php" method="POST" role="form">
 			<div class="form-group">
 				<label for="">Country Name : </label>
-				<input type="text" class="form-control" name="country" placeholder="Input Country Name" value="<?echo $_GET['country']; ?>" required>
+				<input type="text" class="form-control" name="country" id="basics" placeholder="Input Country Name" value="<?echo $_POST['country']; ?>" required>
 			</div>
-			<button type="submit" class="btn btn-primary submtbtn">Submit</button> <a href='index.php'><button type="" class="btn btn-primary submtbtn">Reset</button></a>
+			<button type="submit" class="btn btn-primary submtbtn"'>Submit</button>
 		</form>
 		<?
-if ($_GET['country'] != "") {
-    $country = $_GET['country'];
+if ($_POST['country'] != "") {
+    $country = $_POST['country'];
+    $country = str_replace(' ', '', $country);
     echo "<p><hr>";
     $url      = "https://restcountries.eu/rest/v2/name/" . $country;
     $response = file_get_contents($url);
     $response = json_decode($response);
+    // print_r($response); // ใช้ Array
     $response = $response[0];
     $country  = $response->name;
     if ($response->name != "") {
     	echo "<img src='$response->flag' width='20%' style='float: right'>";
-        echo "<h3>Capital of $country is <b class='coloroftext'>$response->capital</b></h3>";
+        echo "<div class='colorinfo'><h3>Capital of $country is <b class='coloroftext'>$response->capital</b></h3>";
         echo "<p>Region of $country is <b class='coloroftext'>$response->region ($response->subregion)</b></p>";
-        echo "<p>Native Name of $country is <b class='coloroftext'>$response->nativeName</b></p>";
+        echo "<p>Native Name of $country is <b class='coloroftext'>$response->nativeName</b></p>
+        <br><a href='index.php'><button class='btn btn-primary submtbtn'>Reset</button></a></div>";
     } else {
-        echo "<h3>Not Found, Please try agian</h3>";
+        echo "<div class='colorinfo'><h3>Not Found, Please try agian</h3></div>";
     }
 
 }
@@ -59,3 +71,19 @@ if ($_GET['country'] != "") {
 	</div>
 	<div class="col-lg-2"></div>
 </div>
+</body>
+<script type="text/javascript">
+var options = {
+	url: "countries.json",
+
+	getValue: "name",
+
+	list: {
+		match: {
+			enabled: true
+		}
+	}
+};
+
+$("#basics").easyAutocomplete(options);
+</script>
